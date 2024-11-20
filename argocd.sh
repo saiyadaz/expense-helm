3c05984d8cf141b4849603e89c6f7f3964210342
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-20 11:28:39 -0500

    add #!/bin\bash

--- argocd.sh
+++ argocd.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if   [ "$1" == "install" ]; then
 
 kubectl create namespace argocd
@@ -11,6 +13,7 @@ echo Username - admin
 echo Password - $(argocd admin intial-password -n argocd  | head -1)
 
 fi
+
 if   [ "$1" == "jobs" ]; then
     argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password CZsTSo0Q6ZmdxrTY --insecure --grpc-web
 
