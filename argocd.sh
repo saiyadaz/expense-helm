1a4dd96a1919e7684041e0c499fa6d5055eb0237
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-20 11:02:45 -0500

    add argocd script for getiin argocd installation and admin password

--- argocd.sh
+++ argocd.sh
@@ -15,7 +15,7 @@ if   [ "$1" == "jobs" ]; then
     argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
 
       for app in backend frontend ; do
-         argocd app create ${app}  --repo https://github.com/saiyadaz/expense-helm.git --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
+         argocd app create ${app}  --repo https://github.com/saiyadaz/expense-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
       done
     fi
 
