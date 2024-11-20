c012ee2ed24cb775d7d4c9f53c6a52624df238a2
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-20 10:59:26 -0500

    add argocd script for getiin argocd installation and admin password

--- argocd.sh
+++ argocd.sh
@@ -1,3 +1,5 @@
+if   [ "$1" == "install" ]; then
+
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
@@ -7,3 +9,13 @@ sleep 10
 echo URL -https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
 echo Username - admin
 echo Password - $(argocd admin intial-password -n argocd  | head -1)
+
+fi
+if   [ "$1" == "jobs" ]; then
+    argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+
+      for app in backend frontend ; do
+         argocd app create ${app}  --repo https://github.com/saiyadaz/expense-helm.git --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
+      done
+    fi
+
